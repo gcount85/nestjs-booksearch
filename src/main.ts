ed277ee3dd3c27c230babb7b8cb2ffed481ffc8c
gcount85 <100mgml@gmail.com>
2023-08-27 00:40:30 +0900

    feat : Set index.hbs view
    -

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { ConfigService } from '@nestjs/config';
+import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const configService = app.get(ConfigService);
+
+  // set static files
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('hbs');
 
   await app.listen(3000, () => {
     console.log(`Server listening on ${configService.get('serviceUrl')}`);
