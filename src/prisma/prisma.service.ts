efcab95202f23c7a583fec081cbf8f0065444694
gcount85 <100mgml@gmail.com>
2023-09-01 00:56:15 +0900

    feat : Configure prisma dynamic env setting

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,9 +1,19 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 // PrismaClient 인스턴스를 만들고 DB에 연결한다.
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor(private configService: ConfigService) {
+    super({
+      datasources: {
+        db: {
+          url: configService.get('DATABASE_URL'),
+        },
+      },
+    });
+  }
   async onModuleInit() {
     await this.$connect();
   }
